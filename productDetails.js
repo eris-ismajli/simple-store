f188015581ef6e309e15ca1352efaeeabb00f889
eris <erisismajli7@gmail.com>
2026-01-20 20:26:15 +0100

    product details

--- productDetails.js
+++ productDetails.js
@@ -5,3 +5,13 @@ const productId = params.get("id")
 const products = JSON.parse(localStorage.getItem("products"))
 
 const product = products.find(product => product.id === Number(productId))
+
+const productImage = document.getElementById("productImage")
+const productTitle = document.getElementById("productTitle")
+const productPrice = document.getElementById("productPrice")
+const productDesc = document.getElementById("productDesc")
+
+productImage.src = product.image
+productTitle.textContent = product.title
+productPrice.textContent = product.productPrice
+productDesc.textContent = product.description
