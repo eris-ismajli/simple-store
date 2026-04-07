4529317b115e1010f0eaebfeb575c1b47d64a58e
eris <erisismajli7@gmail.com>
2026-04-07 19:51:48 +0200

    feature: add to cart

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 import { products } from "./data/products.js"
 
-localStorage.setItem("products", JSON.stringify(products))
+if (!getProducts()) {
+    localStorage.setItem("products", JSON.stringify(products))
+}
 
-let savedProducts = JSON.parse(localStorage.getItem("products"))
+if (!getCartProducts()) {
+    localStorage.setItem("cartProducts", JSON.stringify([]))
+}
+
+function getProducts() {
+    return JSON.parse(localStorage.getItem("products") || [])
+}
+
+function getCartProducts() {
+    return JSON.parse(localStorage.getItem("cartProducts") || [])
+}
+
+function setCartProducts(newCartProducts) {
+    localStorage.setItem("cartProducts", JSON.stringify(newCartProducts))
+}
+
+const savedProducts = getProducts()
+const savedCartProducts = getCartProducts()
+
 const productsList = document.getElementById("productsList")
+const cartItems = document.getElementById("cartItems")
+
+cartItems.textContent = savedCartProducts.length
 
 function renderProducts() {
     savedProducts.forEach((product, index) => {
@@ -13,9 +36,21 @@ function renderProducts() {
         const productImage = document.createElement("img")
         productImage.src = product.image
 
+        const addToCartBtn = document.createElement("button")
+        addToCartBtn.textContent = "Add to Cart"
+
+        addToCartBtn.addEventListener("click", () => {
+            savedCartProducts.push(product)
+            setCartProducts(savedCartProducts)
+            cartItems.textContent = savedCartProducts.length
+        })
+
         productContainer.appendChild(productImage)
+        productContainer.appendChild(addToCartBtn)
         productsList.appendChild(productContainer)
     })
 }
 
 renderProducts()
+
+// notepad-eris.vercel.app/js-class
